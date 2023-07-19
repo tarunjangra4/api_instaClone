ae8343f13100cbfd06dc6f6523957b8463884db8
Tarun Jangra <tarun.jangra@fincity.com>
2023-07-19 23:04:44 +0530

    Global exception created

---
+++ src/main/java/com/example/demo/exceptions/ErrorDetails.java
@@ -0,0 +1,17 @@
+package com.example.demo.exceptions;
+
+import java.time.LocalDateTime;
+
+public class ErrorDetails {
+	private String message;
+	private String details;
+	private LocalDateTime timestamp;
+	
+	public ErrorDetails(String message, String details, LocalDateTime timestamp) {
+		super();
+		this.message = message;
+		this.details = details;
+		this.timestamp = timestamp;
+	}
+	
+}
