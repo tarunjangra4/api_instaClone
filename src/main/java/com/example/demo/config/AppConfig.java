4fb881549504571c37a71947cd7c832486c0d50f
Tarun Jangra <51834686+tarunjangra4@users.noreply.github.com>
2023-08-08 11:33:00 +0530

    changes with cors error fixes

---
+++ src/main/java/com/example/demo/config/AppConfig.java
@@ -0,0 +1,91 @@
+package com.example.demo.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+@Configuration
+@EnableWebSecurity
+public class AppConfig {
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+		configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+
+	@Bean
+	public SecurityFilterChain securityConfiguration(HttpSecurity http) throws Exception{
+		http
+//				.sessionManagement()
+//				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//				.and()
+			.authorizeHttpRequests()
+			.requestMatchers(HttpMethod.POST,"/api/**").permitAll()
+//			.requestMatchers(HttpMethod.GET,"/**").permitAll()
+//			.requestMatchers(HttpMethod.PUT,"/*").permitAll()
+			// disallow everything else
+			.anyRequest().authenticated()
+			.and()
+			.addFilterBefore(new JwtTokenValidationFilter(), BasicAuthenticationFilter.class)
+			.addFilterAfter(new JwtTokenGeneratorFilter(), BasicAuthenticationFilter.class)
+			.csrf().disable()
+			.cors()
+				.configurationSource(new CorsConfigurationSource() {
+					@Override
+					public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+						CorsConfiguration cfg = new CorsConfiguration();
+						cfg.setAllowedOrigins(Arrays.asList(
+								"http://localhost:3000"
+						));
+						cfg.setAllowedMethods(Collections.singletonList("*"));
+						cfg.setAllowCredentials(true);
+						cfg.setAllowedHeaders(Collections.singletonList("*"));
+						cfg.setExposedHeaders(Arrays.asList("Authorization"));
+						cfg.setMaxAge(3600L);
+						return cfg;
+					}
+				})
+			.and()
+//			.cors(AbstractHttpConfigurer::disable)
+			.httpBasic()
+				.and()
+					.formLogin()
+		;
+
+//		http.cors().and()
+//				.httpBasic().disable()
+//				.csrf().disable()
+//				.authorizeRequests(auth -> auth.m)
+
+		return http.build();
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+}
